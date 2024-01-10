239c2e2d5072d80175d7b80df020cb1515fc4676
Edward Beazer <eddiebeazer@gmail.com>
2024-01-10 16:40:06 -0500

    added more coverage

--- pkgs/fastlane/fastlane_test.go
+++ pkgs/fastlane/fastlane_test.go
@@ -13,12 +13,24 @@ func TestGetGithubReleases(t *testing.T) {
 	}
 
 	assert.Equal(t, *release.TagName, "0.1.3")
+}
 
+func TestGetLatestDraftRelease(t *testing.T) {
 	// Draft Release
-	release, err = GetLatestGithubRelease("eddiebeazer", "unreal-ci", true)
+	release, err := GetLatestGithubRelease("eddiebeazer", "unreal-ci", true)
 	if err != nil {
 		return
 	}
 
 	assert.Equal(t, *release.TagName, "0.1.4")
+}
+
+func TestGetGithubRelease(t *testing.T) {
+	// Draft Release
+	release, err := GetGithubRelease("eddiebeazer", "unreal-ci", "0.1.3")
+	if err != nil {
+		return
+	}
+
+	assert.NotNil(t, release)
 }
